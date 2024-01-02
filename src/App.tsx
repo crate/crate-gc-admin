c57957182a8d74060b8f3a178b81d2c9af8f88b8
Alex Dametto <dametto.alex@gmail.com>
2024-01-02 16:39:02 +0100

    Added crate-ui-components library

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@crate/crate-ui-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button kind={Button.kinds.PRIMARY}>Primary Button</Button>
+      <Button kind={Button.kinds.SECONDARY}>Secondary Button</Button>
+      <Button kind={Button.kinds.TERTIARY}>Tertiary Button</Button>
+      <Button loading>Loading Button</Button>
     </div>
   );
 }
