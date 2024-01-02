f6cad9bd19b1660a2ec231c7ae70997535f2a9bc
Alex Dametto <dametto.alex@gmail.com>
2024-01-02 18:18:57 +0100

    Add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Button } from "@crate/crate-ui-components";
 
 function App() {
   return (
-    <div>
+    <div className="p-4 flex gap-4">
       <Button kind={Button.kinds.PRIMARY}>Primary Button</Button>
       <Button kind={Button.kinds.SECONDARY}>Secondary Button</Button>
       <Button kind={Button.kinds.TERTIARY}>Tertiary Button</Button>
