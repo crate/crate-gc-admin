d58a5aef5260dc132d8097f34f56e248bbb28c32
Romanas Sonkinas <romanass@gmail.com>
2024-01-14 09:58:57 +0000

    Adding support for handling type information
    This can be used for better displaying the results in the SQLResultsTable.

--- src/utils/gc/executeSql.ts
+++ src/utils/gc/executeSql.ts
@@ -5,7 +5,38 @@ type Error = {
   message: string;
 };
 
+enum ColumnType {
+  NULL = 0,
+  NOT_SUPPORTED = 1,
+  CHAR = 2,
+  BOOLEAN = 3,
+  TEXT = 4,
+  IP = 5,
+  DOUBLE = 6,
+  REAL = 7,
+  SMALLINT = 8,
+  INTEGER = 9,
+  BIGINT = 10,
+  TIMESTAMP_WITH_TZ = 11,
+  OBJECT = 12,
+  GEOPOINT = 13,
+  GEOSHAPE = 14,
+  TIMESTAMP_WITHOUT_TZ = 15,
+  UNCHECKED_OBJECT = 16,
+  REGPROC = 19,
+  TIME = 20,
+  OIDVECTOR = 21,
+  NUMERIC = 22,
+  REGCLASS = 23,
+  DATE = 24,
+  BIT = 25,
+  JSON = 26,
+  CHARACTER = 27,
+  ARRAY = 100,
+}
+
 type QueryResult = object & {
+  col_types: ColumnType[] | undefined;
   cols: string[];
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   rows: any[][];
@@ -43,4 +74,5 @@ async function executeSql(
 }
 
 export type { Error, QueryResult, QueryResults };
+export { ColumnType };
 export default executeSql;
